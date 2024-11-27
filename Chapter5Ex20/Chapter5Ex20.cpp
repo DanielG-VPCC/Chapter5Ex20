50eab2b0624b6fb7a3554b5927c6309c60b86cab
Daniel <Daniel@GERALD>
2024-11-27 14:31:57 -0500

    Changed the description to focus on Exercise 21, and amended the code to show the amount of tries the user took to guess the correct answer

--- Chapter5Ex20/Chapter5Ex20.cpp
+++ Chapter5Ex20/Chapter5Ex20.cpp
@@ -1,8 +1,8 @@
 /*
-Title:Chapter 5 Exercise 20 - Random Number Guessing Game
+Title:Chapter 5 Exercise 21 - Random Number Guessing Game Part 2
 File name:Chapter5Ex20_Daniel_Geesaman.cpp
 Programmer:Daniel Geesaman
-Date:11/24/2024
+Date:11/27/2024
 Requirements:
 
 Write a program that generates a random number and asks the user to guess what the number is.
@@ -12,6 +12,15 @@ If the user’s guess is higher than the random number, the program should display
 If the user’s guess is lower than the random number, the program should display “Too low, try again.”
 
 The program should use a loop that repeats until the user correctly guesses the random number.
+
+
+EXERCISE 21 REQUIREMENTS BELOW:
+
+21. Create a second branch in your Exercise 20 project and Enhance the program that you wrote for Programming Challenge 20
+
+so it keeps a count of the number of guesses the user makes. When the user correctly guesses the random number,
+
+the program should display the number of guesses.
 */
 
 #include <iostream>
@@ -25,13 +34,18 @@ void resultOutput(int Guess, int Answer);
 
 int main()
 {
+	int tries = 0;
 	int Answer = randNumGen();
 	while (true)
 	{
+		tries++;
 		int Guess = userGuess();
 		resultOutput(Guess, Answer);
 		if (Answer == Guess)
+		{
+			cout << "You took " << tries << " tries to guess the correct answer.";
 			return false;
+		}
 	}
 }
 
